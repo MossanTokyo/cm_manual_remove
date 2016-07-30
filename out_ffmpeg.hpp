c83fc180e8e5315034916a895f9ab1af3ae56aaf
MossanTokyo <mossan.tokyo.new@gmail.com>
2016-07-30 17:58:43 +0900

    Update out_ffmpeg.hpp
    modified to generate a batch script for a video without any CM.

--- out_ffmpeg.hpp
+++ out_ffmpeg.hpp
@@ -6,26 +6,34 @@
 
 void trim(fs::ofstream & out, fs::path & file, std::vector<std::pair<int, int>> & contents, fs::path ffmpeg_path, std::string ffmpeg_options)
 {
-	int i = 0;
-
-	int gap = 3;
-
-	// trim command
-	for (auto & elem : contents)
+	if (contents.size() != 0)
 	{
-		out << ffmpeg_path << " -y -ss " << (((elem.first - gap) < 0) ? 0 : elem.first - gap) << " -i \"" << file.string() << "\" -t " << elem.second - elem.first + gap << "  -vcodec copy -acodec copy tmp_" + std::to_string(i++) + ".m2ts\n";
+		int i = 0;
+
+		int gap = 3;
+
+		// trim command
+		for (auto & elem : contents)
+		{
+			out << ffmpeg_path << " -y -ss " << (((elem.first - gap) < 0) ? 0 : elem.first - gap) << " -i \"" << file.string() << "\" -t " << elem.second - elem.first + gap << "  -vcodec copy -acodec copy tmp_" + std::to_string(i++) + ".m2ts\n";
+		}
+
+		// concastrate command
+
+		// ffmpeg path
+		out << ffmpeg_path;
+
+		for (auto j = 0; j < i; ++j)
+		{
+			out << " -i tmp_" << std::to_string(j) << ".m2ts";
+		}
+
+		// ffmpeg commands
+		out << " -filter_complex \"yadif=0:-1,concat=n=" << std::to_string(i) << ":v=1:a=1\" " << ffmpeg_options << " \"done\\" << file.stem().string() << ".mp4\"\n";
 	}
-
-	// concastrate command
-
-	// ffmpeg path
-	out << ffmpeg_path;
-
-	for (auto j = 0; j < i; ++j)
+	else
 	{
-		out << " -i tmp_" << std::to_string(j) << ".m2ts";
+		out << ffmpeg_path << " ";
+		out << file.string() << " -filter_complex \"yadif=0:-1 " << ffmpeg_options << " \"done\\" << file.stem().string() << ".mp4\"\n";
 	}
-
-	// ffmpeg commands
-	out << " -filter_complex \"yadif=0:-1,concat=n=" << std::to_string(i) << ":v=1:a=1\" " << ffmpeg_options << " \"done\\" << file.stem().string() << ".mp4\"\n";
 }
